5d0ce191669346ca84d4d8a13e75c7cf5455f678
datoonman <73767324+datoonman@users.noreply.github.com>
2024-08-05 21:47:28 +0700

    Update script.js
    updated link

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-fetch('https://api.countapi.xyz/hit/datoonman/visit-counter')
+fetch('https://api.countapi.xyz/hit/datoonman/suslink')
     .then(response => response.json())
     .then(data => {
         document.getElementById('counter').textContent = data.value;
-    });
+    })
+    .catch(error => console.error('Error:', error));
